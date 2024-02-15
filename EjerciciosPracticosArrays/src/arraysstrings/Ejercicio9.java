35fd5d14d1065d39d4264b6904ccd0066912f329
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2024-02-15 21:01:13 +0100

    Ejercicios

--- EjerciciosPracticosArrays/src/arraysstrings/Ejercicio9.java
+++ EjerciciosPracticosArrays/src/arraysstrings/Ejercicio9.java
@@ -4,15 +4,37 @@ import java.util.Scanner;
 
 public class Ejercicio9 {
 
+	private static final String COMIENZO = "Javalín, javalón";
+	private static final String FINALIZACION = "javalén, lén, lén";
+
 	public static void main(String[] args) {
 
-		String frase;
-		
-		Scanner sc = new Scanner (System.in);
-		
-		System.out.println("Introduce una frase --> ");
+		Scanner sc = new Scanner(System.in);
 
-		frase = sc.nextLine();
+		String fraseJavalandia = "";
+
+		String fraseTraducida = "";
+
+		System.out.println("Introduce la frase");
+		fraseJavalandia = sc.nextLine();
+
+		if (fraseJavalandia.startsWith(COMIENZO)) {
+
+			fraseTraducida = fraseJavalandia.replace(COMIENZO, "");
+
+		} else if (fraseJavalandia.endsWith(FINALIZACION)) {
+
+			fraseTraducida = fraseJavalandia.replace(FINALIZACION, "");
+
+		} else {
+			fraseTraducida = fraseJavalandia;
+		}
+
+		fraseTraducida = fraseTraducida.trim();
+
+		System.out.println(fraseTraducida);
+
+		sc.close();
 	}
 
 }
