f4b8c47aa42d3873402d62c3450d3644b2f2d0bd
jlbarrionuevo <jlbarrionuevo@106-06.info2>
2024-02-15 14:10:02 +0100

    Ejercicios

---
+++ EjerciciosPracticosArrays/src/arraysstrings/Ejercicio9.java
@@ -0,0 +1,18 @@
+package arraysstrings;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+
+	public static void main(String[] args) {
+
+		String frase;
+		
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Introduce una frase --> ");
+
+		frase = sc.nextLine();
+	}
+
+}
