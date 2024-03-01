cdf0f23bc0182d9ef426e82dcdb405d513c2889b
jlbarrionuevo <jlbarrionuevo@106-06.info2>
2024-03-01 08:35:51 +0100

    ej

--- EjerciciosPracticosArrays/src/arraysstrings/Ejercicio17.java
+++ EjerciciosPracticosArrays/src/arraysstrings/Ejercicio17.java
@@ -1,8 +1,39 @@
 package arraysstrings;
 
+import java.util.Scanner;
+
 public class Ejercicio17 {
+
+	public static final String FIN = "*/";
+	public static final String INICIO = "*/";
 
 	public static void main(String[] args) {
 
+		String codigoC;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduce una frase --> ");
+		codigoC = sc.nextLine();
+		
+		System.out.println(sinComentarios(codigoC));
+		
+	}
+	
+	public static String sinComentarios(String codigoC) {
+		String cadSinComentarios = "";
+		int inicio;
+		int fin;
+		
+		inicio = codigoC.indexOf(INICIO);
+		fin = codigoC.indexOf(FIN);
+		
+		while (inicio != -1) {
+			cadSinComentarios += codigoC.substring(fin, inicio);
+			fin = codigoC.indexOf(FIN, inicio)+FIN.length();
+			inicio = codigoC.indexOf(INICIO, fin);
+		}
+		
+		return cadSinComentarios;
 	}
 }
